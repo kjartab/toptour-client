2929a01b7cf58fe2259d0651eda934cd66d3d10e
kjartab <kjartanbjorset@gmail.com>
2017-04-22 11:24:43 +0200

    start reactgl-js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,12 @@
-// import moment from 'moment';
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-// import AutoComplete from 'material-ui/AutoComplete';
+import ReactDOM from 'react-dom'; 
+
 import SearchBox from './components/Search.jsx'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import * as name from "./module1";
-import injectTapEventPlugin from 'react-tap-event-plugin';
-// var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
-// console.log(rightNow);
-console.log("ok");
-console.log("2ssssss3s")
-console.log(name.testFunc(),23);
-var k = 2;
-var t = 2;
+import injectTapEventPlugin from 'react-tap-event-plugin'; 
 
-var t = 2;
-
- injectTapEventPlugin();
+injectTapEventPlugin();
 
 class App extends Component {
     render() {
